5f938a93c3e8c903aef3ebbab9371aa2002fd5c3
Tim Oesterreich <tim.oesterreich1@gmail.com>
2019-01-22 18:32:56 +0100

    prelim

--- src/lib/asio/asio_stream.h
+++ src/lib/asio/asio_stream.h
@@ -5,9 +5,9 @@
 #include <botan/asio_async_read_op.h>
 #include <botan/asio_async_write_op.h>
 #include <botan/asio_convert_exceptions.h>
+#include <botan/asio_includes.h>
 #include <botan/asio_stream_base.h>
 #include <botan/asio_stream_core.h>
-#include <botan/asio_includes.h>
 
 #include <memory>
 #include <thread>
@@ -16,239 +16,227 @@
 namespace Botan {
 
 /**
-* boost::asio compatible SSL/TLS stream based on TLS::Client or TLS::Server.
-*/
+ * boost::asio compatible SSL/TLS stream based on TLS::Client or TLS::Server.
+ */
 template <class StreamLayer, class Channel>
-class Stream : public StreamBase<Channel>
-   {
-   public:
-      using next_layer_type = typename std::remove_reference<StreamLayer>::type;
+class Stream : public StreamBase<Channel> {
+public:
+  using next_layer_type = typename std::remove_reference<StreamLayer>::type;
 
-      using lowest_layer_type = typename next_layer_type::lowest_layer_type;
+  using lowest_layer_type = typename next_layer_type::lowest_layer_type;
 
-      using executor_type = typename lowest_layer_type::executor_type;
+  using executor_type = typename lowest_layer_type::executor_type;
 
-      template <typename... Args>
-      Stream(StreamLayer nextLayer, Args&& ... args)
-         : StreamBase<Channel>(std::forward<Args>(args)...)
-         , nextLayer_(std::forward<StreamLayer>(nextLayer))
-         {
-         }
+  template <typename... Args>
+  Stream(StreamLayer nextLayer, Args &&... args)
+      : StreamBase<Channel>(std::forward<Args>(args)...),
+        nextLayer_(std::forward<StreamLayer>(nextLayer)) {}
 
-      executor_type get_executor() noexcept { return nextLayer_.get_executor(); }
+  executor_type get_executor() noexcept { return nextLayer_.get_executor(); }
 
-      const next_layer_type& next_layer() const { return nextLayer_; }
-      next_layer_type&       next_layer() { return nextLayer_; }
+  const next_layer_type &next_layer() const { return nextLayer_; }
+  next_layer_type &next_layer() { return nextLayer_; }
 
-      lowest_layer_type& lowest_layer() { return nextLayer_.lowest_layer(); }
+  lowest_layer_type &lowest_layer() { return nextLayer_.lowest_layer(); }
 
-      Channel& channel() { return this->channel_; }
+  Channel &channel() { return this->channel_; }
 
-      const Channel& channel() const { return this->channel_; }
+  const Channel &channel() const { return this->channel_; }
 
-      void handshake(boost::system::error_code& ec)
-         {
-         try
-            {
-            handshake();
-            }
-         catch(...)
-            {
-            ec = Botan::convertException();
-            return;
-            }
-         }
+  void handshake(boost::system::error_code &ec) {
+    try {
+      handshake();
+    } catch (...) {
+      ec = Botan::convertException();
+      return;
+    }
+  }
 
-      void handshake()
-         {
-         boost::system::error_code ec;
-         while(!channel().is_active())
-            {
-            writePendingTlsData();
+  void handshake() {
+    boost::system::error_code ec;
+    while (!channel().is_active()) {
+      writePendingTlsData();
 
-            auto read_buffer =
-               boost::asio::buffer(this->core_.input_buffer_, nextLayer_.read_some(this->core_.input_buffer_, ec));
-            boost::asio::detail::throw_error(ec, "handshake");
+      auto read_buffer = boost::asio::buffer(
+          this->core_.input_buffer_,
+          nextLayer_.read_some(this->core_.input_buffer_, ec));
+      boost::asio::detail::throw_error(ec, "handshake");
 
-            channel().received_data(static_cast<const uint8_t*>(read_buffer.data()), read_buffer.size());
+      channel().received_data(static_cast<const uint8_t *>(read_buffer.data()),
+                              read_buffer.size());
 
-            writePendingTlsData();
-            }
-         }
+      writePendingTlsData();
+    }
+  }
 
-      template <typename HandshakeHandler>
-      BOOST_ASIO_INITFN_RESULT_TYPE(HandshakeHandler, void(boost::system::error_code))
-      async_handshake(HandshakeHandler&& handler)
-         {
-         // If you get an error on the following line it means that your handler does not meet the documented type
-         // requirements for a HandshakeHandler.
-         BOOST_ASIO_HANDSHAKE_HANDLER_CHECK(HandshakeHandler, handler) type_check;
+  template <typename HandshakeHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(HandshakeHandler,
+                                void(boost::system::error_code))
+  async_handshake(HandshakeHandler &&handler) {
+    // If you get an error on the following line it means that your handler does
+    // not meet the documented type requirements for a HandshakeHandler.
+    BOOST_ASIO_HANDSHAKE_HANDLER_CHECK(HandshakeHandler, handler) type_check;
 
-         boost::asio::async_completion<HandshakeHandler, void(boost::system::error_code)> init(handler);
+    boost::asio::async_completion<HandshakeHandler,
+                                  void(boost::system::error_code)>
+        init(handler);
 
-         auto op = create_async_handshake_op(std::move(init.completion_handler));
-         op(boost::system::error_code{}, 0, 1);
+    auto op = create_async_handshake_op(std::move(init.completion_handler));
+    op(boost::system::error_code{}, 0, 1);
 
-         return init.result.get();
-         }
+    return init.result.get();
+  }
 
-      void shutdown(boost::system::error_code& ec)
-         {
-         try
-            {
-            shutdown();
-            }
-         catch(...)
-            {
-            ec = Botan::convertException();
-            return;
-            }
-         }
+  void shutdown(boost::system::error_code &ec) {
+    try {
+      shutdown();
+    } catch (...) {
+      ec = Botan::convertException();
+      return;
+    }
+  }
 
-      void shutdown()
-         {
-         channel().close();
-         writePendingTlsData();
-         }
+  void shutdown() {
+    channel().close();
+    writePendingTlsData();
+  }
 
-      template <typename MutableBufferSequence>
-      std::size_t read_some(const MutableBufferSequence& buffers, boost::system::error_code& ec)
-         {
-         try
-            {
-            return read_some(buffers);
-            }
-         catch(...)
-            {
-            ec = Botan::convertException();
-            return 0;
-            }
-         }
+  template <typename MutableBufferSequence>
+  std::size_t read_some(const MutableBufferSequence &buffers,
+                        boost::system::error_code &ec) {
+    // TODO(tim): Error handling
+    if (this->core_.hasReceivedData()) {
+      return this->core_.copyReceivedData(buffers, ec);
+    }
 
-      template <typename MutableBufferSequence>
-      std::size_t read_some(const MutableBufferSequence& buffers)
-         {
-         boost::system::error_code ec;
-         if(this->core_.hasReceivedData())
-            {
-            return this->core_.copyReceivedData(buffers);
-            }
+    if (this->core_.finished_reading_record_) {
+      ec = boost::asio::error::eof;
+      return 0;
+    }
 
-         auto read_buffer = boost::asio::buffer(
-                               this->core_.input_buffer_,
-                               nextLayer_.read_some(this->core_.input_buffer_, ec));
-         boost::asio::detail::throw_error(ec, "read_some");
+    auto read_buffer = boost::asio::buffer(
+        this->core_.input_buffer_,
+        nextLayer_.read_some(this->core_.input_buffer_, ec));
 
-         channel().received_data(static_cast<const uint8_t*>(read_buffer.data()),
-                                 read_buffer.size());
-         return this->core_.copyReceivedData(buffers);
-         }
+    boost::asio::detail::throw_error(ec, "read_some");
 
-      template <typename ConstBufferSequence>
-      std::size_t write_some(const ConstBufferSequence& buffers, boost::system::error_code& ec)
-         {
-         try
-            {
-            return write_some(buffers);
-            }
-         catch(...)
-            {
-            ec = Botan::convertException();
-            return 0;
-            }
-         }
+    channel().received_data(static_cast<const uint8_t *>(read_buffer.data()),
+                            read_buffer.size());
+    return this->core_.copyReceivedData(buffers, ec);
+  }
 
-      template <typename ConstBufferSequence>
-      std::size_t write_some(const ConstBufferSequence& buffers)
-         {
-         boost::asio::const_buffer buffer =
-            boost::asio::detail::buffer_sequence_adapter<
+  template <typename MutableBufferSequence>
+  std::size_t read_some(const MutableBufferSequence &buffers) {
+    boost::system::error_code ec;
+    return read_some(buffers, ec);
+  }
+
+  template <typename ConstBufferSequence>
+  std::size_t write_some(const ConstBufferSequence &buffers,
+                         boost::system::error_code &ec) {
+    try {
+      return write_some(buffers);
+    } catch (...) {
+      ec = Botan::convertException();
+      return 0;
+    }
+  }
+
+  template <typename ConstBufferSequence>
+  std::size_t write_some(const ConstBufferSequence &buffers) {
+    boost::asio::const_buffer buffer =
+        boost::asio::detail::buffer_sequence_adapter<
             boost::asio::const_buffer, ConstBufferSequence>::first(buffers);
 
-         channel().send(static_cast<const uint8_t*>(buffer.data()), buffer.size());
-         writePendingTlsData();
-         return buffer.size();
-         }
+    channel().send(static_cast<const uint8_t *>(buffer.data()), buffer.size());
+    writePendingTlsData();
+    return buffer.size();
+  }
 
-      template <typename ConstBufferSequence, typename WriteHandler>
-      BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler, void(boost::system::error_code, std::size_t))
-      async_write_some(const ConstBufferSequence& buffers, WriteHandler&& handler)
-         {
-         BOOST_ASIO_WRITE_HANDLER_CHECK(WriteHandler, handler) type_check;
+  template <typename ConstBufferSequence, typename WriteHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler,
+                                void(boost::system::error_code, std::size_t))
+  async_write_some(const ConstBufferSequence &buffers, WriteHandler &&handler) {
+    BOOST_ASIO_WRITE_HANDLER_CHECK(WriteHandler, handler) type_check;
 
-         boost::asio::const_buffer buffer =
-            boost::asio::detail::buffer_sequence_adapter<
+    boost::asio::const_buffer buffer =
+        boost::asio::detail::buffer_sequence_adapter<
             boost::asio::const_buffer, ConstBufferSequence>::first(buffers);
 
-         try
-            {
-            channel().send(static_cast<const uint8_t*>(buffer.data()), buffer.size());
-            }
-         catch(...)
-            {
-            // TODO: don't call directly
-            handler(Botan::convertException(), 0);
-            return;
-            }
+    try {
+      channel().send(static_cast<const uint8_t *>(buffer.data()),
+                     buffer.size());
+    } catch (...) {
+      // TODO: don't call directly
+      handler(Botan::convertException(), 0);
+      return;
+    }
 
-         boost::asio::async_completion<WriteHandler, void(boost::system::error_code, std::size_t)> init(handler);
-         auto op = create_async_write_op(std::move(init.completion_handler), buffer.size());
+    boost::asio::async_completion<WriteHandler,
+                                  void(boost::system::error_code, std::size_t)>
+        init(handler);
+    auto op = create_async_write_op(std::move(init.completion_handler),
+                                    buffer.size());
 
-         boost::asio::async_write(nextLayer_, this->core_.sendBuffer(), std::move(op));
-         return init.result.get();
-         }
+    boost::asio::async_write(nextLayer_, this->core_.sendBuffer(),
+                             std::move(op));
+    return init.result.get();
+  }
 
-      template <typename MutableBufferSequence, typename ReadHandler>
-      BOOST_ASIO_INITFN_RESULT_TYPE(ReadHandler, void(boost::system::error_code, std::size_t))
-      async_read_some(const MutableBufferSequence& buffers, ReadHandler&& handler)
-         {
-         BOOST_ASIO_READ_HANDLER_CHECK(ReadHandler, handler) type_check;
+  template <typename MutableBufferSequence, typename ReadHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(ReadHandler,
+                                void(boost::system::error_code, std::size_t))
+  async_read_some(const MutableBufferSequence &buffers, ReadHandler &&handler) {
+    BOOST_ASIO_READ_HANDLER_CHECK(ReadHandler, handler) type_check;
 
-         boost::asio::async_completion<ReadHandler, void(boost::system::error_code, std::size_t)> init(handler);
+    boost::asio::async_completion<ReadHandler,
+                                  void(boost::system::error_code, std::size_t)>
+        init(handler);
 
-         auto op = create_async_read_op(std::move(init.completion_handler), buffers);
-         op(boost::system::error_code{}, 0, /* first_call */ true);
-         return init.result.get();
-         }
+    auto op = create_async_read_op(std::move(init.completion_handler), buffers);
+    op(boost::system::error_code{}, 0, /* first_call */ true);
+    return init.result.get();
+  }
 
-   protected:
-      size_t writePendingTlsData()
-         {
-         boost::system::error_code ec;
-         auto writtenBytes = boost::asio::write(nextLayer_, this->core_.sendBuffer(), ec);
-         boost::asio::detail::throw_error(ec, "writePendingTlsData");
+protected:
+  size_t writePendingTlsData() {
+    boost::system::error_code ec;
+    auto writtenBytes =
+        boost::asio::write(nextLayer_, this->core_.sendBuffer(), ec);
+    boost::asio::detail::throw_error(ec, "writePendingTlsData");
 
-         this->core_.consumeSendBuffer(writtenBytes);
-         return writtenBytes;
-         }
+    this->core_.consumeSendBuffer(writtenBytes);
+    return writtenBytes;
+  }
 
-      template <typename Handler>
-      Botan::AsyncHandshakeOperation<Channel, StreamLayer, Handler>
-      create_async_handshake_op(Handler&& handler)
-         {
-         return Botan::AsyncHandshakeOperation<Channel, StreamLayer, Handler>(
-                   channel(), this->core_, nextLayer_, std::forward<Handler>(handler));
-         }
+  template <typename Handler>
+  Botan::AsyncHandshakeOperation<Channel, StreamLayer, Handler>
+  create_async_handshake_op(Handler &&handler) {
+    return Botan::AsyncHandshakeOperation<Channel, StreamLayer, Handler>(
+        channel(), this->core_, nextLayer_, std::forward<Handler>(handler));
+  }
 
-      template <typename Handler, typename MutableBufferSequence>
-      Botan::AsyncReadOperation<Channel, StreamLayer, Handler, MutableBufferSequence>
-      create_async_read_op(Handler&& handler, const MutableBufferSequence& buffers)
-         {
-         return Botan::AsyncReadOperation<Channel, StreamLayer, Handler, MutableBufferSequence>(
-                   channel(), this->core_, nextLayer_, std::forward<Handler>(handler), buffers);
-         }
+  template <typename Handler, typename MutableBufferSequence>
+  Botan::AsyncReadOperation<Channel, StreamLayer, Handler,
+                            MutableBufferSequence>
+  create_async_read_op(Handler &&handler,
+                       const MutableBufferSequence &buffers) {
+    return Botan::AsyncReadOperation<Channel, StreamLayer, Handler,
+                                     MutableBufferSequence>(
+        channel(), this->core_, nextLayer_, std::forward<Handler>(handler),
+        buffers);
+  }
 
-      template <typename Handler>
-      Botan::AsyncWriteOperation<Handler>
-      create_async_write_op(Handler&& handler, std::size_t plainBytesTransferred)
-         {
-         return Botan::AsyncWriteOperation<Handler>(
-                   this->core_, std::forward<Handler>(handler), plainBytesTransferred);
-         }
+  template <typename Handler>
+  Botan::AsyncWriteOperation<Handler>
+  create_async_write_op(Handler &&handler, std::size_t plainBytesTransferred) {
+    return Botan::AsyncWriteOperation<Handler>(
+        this->core_, std::forward<Handler>(handler), plainBytesTransferred);
+  }
 
-      StreamLayer nextLayer_;
-   };
+  StreamLayer nextLayer_;
+};
 
-}  // namespace botan
+} // namespace Botan
 
 #endif
--- src/lib/asio/asio_stream_core.h
+++ src/lib/asio/asio_stream_core.h
@@ -1,8 +1,8 @@
 #ifndef BOTAN_ASIO_STREAM_CORE_H_
 #define BOTAN_ASIO_STREAM_CORE_H_
 
-#include <botan/tls_callbacks.h>
 #include <botan/asio_includes.h>
+#include <botan/tls_callbacks.h>
 #include <mutex>
 #include <vector>
 
@@ -10,87 +10,92 @@ namespace Botan {
 /**
  * Contains the buffers for reading/sending, and the needed botan callbacks
  */
-struct StreamCore : public Botan::TLS::Callbacks
-   {
-      struct Buffer
-         {
-         Buffer() : dynamicBuffer(data_buffer) {}
-         std::vector<uint8_t> data_buffer;
-         boost::asio::dynamic_vector_buffer<uint8_t, typename decltype(data_buffer)::allocator_type> dynamicBuffer;
-         };
+struct StreamCore : public Botan::TLS::Callbacks {
+  struct Buffer {
+    Buffer() : dynamicBuffer(data_buffer) {}
+    std::vector<uint8_t> data_buffer;
+    boost::asio::dynamic_vector_buffer<
+        uint8_t, typename decltype(data_buffer)::allocator_type>
+        dynamicBuffer;
+  };
 
-      StreamCore()
-         : input_buffer_space_(17 * 1024, '\0'),  // enough for a TLS Datagram
-           input_buffer_(boost::asio::buffer(input_buffer_space_)) {}
+  StreamCore()
+      : input_buffer_space_(17 * 1024, '\0'), // enough for a TLS Datagram
+        input_buffer_(boost::asio::buffer(input_buffer_space_)),
+        finished_reading_record_(false) {}
 
-      virtual ~StreamCore() = default;
+  virtual ~StreamCore() = default;
 
-      void tls_emit_data(const uint8_t data[], size_t size) override
-         {
-         auto buffer = send_buffer_.dynamicBuffer.prepare(size);
-         auto copySize = boost::asio::buffer_copy(buffer, boost::asio::buffer(data, size));
-         send_buffer_.dynamicBuffer.commit(copySize);
-         }
+  void tls_emit_data(const uint8_t data[], size_t size) override {
+    auto buffer = send_buffer_.dynamicBuffer.prepare(size);
+    auto copySize =
+        boost::asio::buffer_copy(buffer, boost::asio::buffer(data, size));
+    send_buffer_.dynamicBuffer.commit(copySize);
+  }
 
-      void tls_record_received(uint64_t, const uint8_t data[], size_t size) override
-         {
-         auto buffer = receive_buffer_.dynamicBuffer.prepare(size);
-         auto copySize = boost::asio::buffer_copy(buffer, boost::asio::buffer(data, size));
-         receive_buffer_.dynamicBuffer.commit(copySize);
-         }
+  void tls_record_received(uint64_t, const uint8_t data[],
+                           size_t size) override {
+    auto buffer = receive_buffer_.dynamicBuffer.prepare(size);
+    auto copySize =
+        boost::asio::buffer_copy(buffer, boost::asio::buffer(data, size));
+    std::cout << "Received size: " << size << ", copySize: " << copySize
+              << std::endl;
+    receive_buffer_.dynamicBuffer.commit(copySize);
+  }
 
-      void tls_alert(Botan::TLS::Alert alert) override
-         {
-         if(alert.type() == Botan::TLS::Alert::CLOSE_NOTIFY)
-            {
-               // TODO
-            }
-         }
+  void tls_alert(Botan::TLS::Alert alert) override {
+    if (alert.type() == Botan::TLS::Alert::CLOSE_NOTIFY) {
+      // TODO
+    }
+  }
 
-       std::chrono::milliseconds tls_verify_cert_chain_ocsp_timeout() const override{
-          return std::chrono::milliseconds(1000);
-       }
+  std::chrono::milliseconds
+  tls_verify_cert_chain_ocsp_timeout() const override {
+    return std::chrono::milliseconds(1000);
+  }
 
-      bool tls_session_established(const Botan::TLS::Session&) override
-         {
-         return true;
-         }
+  bool tls_session_established(const Botan::TLS::Session &) override {
+    return true;
+  }
 
-      bool hasReceivedData() const
-         {
-         return receive_buffer_.dynamicBuffer.size() > 0;
-         }
+  bool hasReceivedData() const {
+    return receive_buffer_.dynamicBuffer.size() > 0;
+  }
 
-      template <typename MutableBufferSequence>
-      std::size_t copyReceivedData(MutableBufferSequence buffers)
-         {
-         auto copiedBytes = boost::asio::buffer_copy(buffers, receive_buffer_.dynamicBuffer.data());
-         receive_buffer_.dynamicBuffer.consume(copiedBytes);
-         return copiedBytes;
-         }
+  template <typename MutableBufferSequence>
+  std::size_t copyReceivedData(MutableBufferSequence buffers,
+                               boost::system::error_code &ec) {
+    auto copiedBytes =
+        boost::asio::buffer_copy(buffers, receive_buffer_.dynamicBuffer.data());
+    receive_buffer_.dynamicBuffer.consume(copiedBytes);
+    if (receive_buffer_.dynamicBuffer.size() == 0) {
+      finished_reading_record_ = true;
+    }
+    return copiedBytes;
+  }
 
-      bool hasDataToSend() const { return send_buffer_.dynamicBuffer.size() > 0; }
+  bool hasDataToSend() const { return send_buffer_.dynamicBuffer.size() > 0; }
 
-      boost::asio::const_buffer sendBuffer() const
-         {
-         return send_buffer_.dynamicBuffer.data();
-         }
+  boost::asio::const_buffer sendBuffer() const {
+    return send_buffer_.dynamicBuffer.data();
+  }
 
-      void consumeSendBuffer(std::size_t bytesConsumed)
-         {
-         send_buffer_.dynamicBuffer.consume(bytesConsumed);
-         }
+  void consumeSendBuffer(std::size_t bytesConsumed) {
+    send_buffer_.dynamicBuffer.consume(bytesConsumed);
+  }
 
-      // Buffer space used to read input intended for the engine.
-      std::vector<uint8_t> input_buffer_space_;
+  // Buffer space used to read input intended for the engine.
+  std::vector<uint8_t> input_buffer_space_;
 
-      // A buffer that may be used to read input intended for the engine.
-      const boost::asio::mutable_buffer input_buffer_;
+  // A buffer that may be used to read input intended for the engine.
+  const boost::asio::mutable_buffer input_buffer_;
 
-   private:
-      Buffer receive_buffer_;
-      Buffer send_buffer_;
-   };
-}  // namespace Botan
+  bool finished_reading_record_;
+
+private:
+  Buffer receive_buffer_;
+  Buffer send_buffer_;
+};
+} // namespace Botan
 
 #endif
